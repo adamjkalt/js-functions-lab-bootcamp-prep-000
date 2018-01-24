e3217b1bc7396fbc470209372010d78813617459
Adam Kalt <adam.kalt.1@gmail.com>
2018-01-24 19:32:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function happyHolidayTo(string1, string2) {
   return `Happy ${string1}, ${string2}!`;
 }
 
-function holidayCountdown(string1, string2) {
+function holidayCountdhttps://learn.co/tracks/bootcamp-prep/javascript-fundamentals/functions-and-scope/functions-lab?batch_id=460&track_id=19381#own(string1, string2) {
   return `It's ${days} days until ${holiday}!`;
 }
